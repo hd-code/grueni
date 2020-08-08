cbd2c05b1729fbb2d8641d250cdd066728221a92
hd-code <54773293+hd-code@users.noreply.github.com>
2020-08-08 17:43:53 +0200

    Dev hd (#6)
    * further developed backend
    
    * added dialog
    
    * built basic structure and first api frontend calls
    
    * further development of backend
    
    Co-authored-by: Hannes Dröse <hd@HDs-MacBook-Pro.local>
    Co-authored-by: Hannes Dröse <hd@HDs-MacBook-Pro.fritz.box>

--- frontend/assets/components/dialog.js
+++ frontend/assets/components/dialog.js
@@ -24,22 +24,22 @@ Vue.component('a-dialog', {
     }, 
     props: [ 'position', 'text' ],
     template: `<a-entity :position="position">
-                    <a-entity
-                        :scale="calcWidth + ' ' + calcHeight + ' 1'"
-                        geometry="primitive: circle; radius: 0.5;" :material="'color: ' + backgroundColor">
-                    </a-entity>
-                    <a-text
-                        :value="text"
-                        xOffset="1.5"
-                        :color="textColor" :wrap-count="charsPerLine"
-                        align="center" :height="height" :width="width - 0.75">
-                    </a-text>
-                    <a-entity
-                        geometry="primitive: plane; 
-                        width: 0.5; height: 0.5;" 
-                        :material="'color: ' + backgroundColor"
-                        rotation="0 0 60"
-                        :position="boxPosition">
-                    </a-entity>
-                </a-entity>`,
+        <a-entity
+            :scale="calcWidth + ' ' + calcHeight + ' 1'"
+            geometry="primitive: circle; radius: 0.5;" :material="'color: ' + backgroundColor">
+        </a-entity>
+        <a-text
+            :value="text"
+            xOffset="1.5"
+            :color="textColor" :wrap-count="charsPerLine"
+            align="center" :height="height" :width="width - 0.75">
+        </a-text>
+        <a-entity
+            geometry="primitive: plane; 
+            width: 0.5; height: 0.5;" 
+            :material="'color: ' + backgroundColor"
+            rotation="0 0 60"
+            :position="boxPosition">
+        </a-entity>
+    </a-entity>`,
 });
